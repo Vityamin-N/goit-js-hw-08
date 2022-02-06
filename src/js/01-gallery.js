7417736f1573af986fc6bf10e1eaaf64be60ff06
Vityamin-N <heartless41196@gmail.com>
2022-02-06 19:33:30 +0200

    Completed all tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+const galleryGrid = galleryItems.map(createItemsMarkup).join('');
+
+galleryRef.innerHTML = galleryGrid;
+
+const lightboxOptions = {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+let gallery = new SimpleLightbox('.gallery a', lightboxOptions);
+
+function createItemsMarkup({ preview, original, description }) {
+  return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const items = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form [name="email"]'),
+  message: document.querySelector('.feedback-form [name="message"]'),
+};
+const STORAGE_FEEDBACK_FORM = 'feedback-form-state';
+const throttledOnFeedbackFormInput = throttle(onFeedbackFormInput, 500);
+let formState = {};
 
+if (localStorage[STORAGE_FEEDBACK_FORM]) {
+  try {
+    formState = JSON.parse(localStorage[STORAGE_FEEDBACK_FORM]);
+  } catch {
+    formState.email = '';
+    formState.message = '';
+  }
+  items.email.value = formState.email ? formState.email : '';
+
+  items.message.value = formState.message ? formState.message : '';
+}
+
+items.form.addEventListener('input', throttledOnFeedbackFormInput);
+items.form.addEventListener('submit', onFeedbackFormSubmit);
+
+function onFeedbackFormInput(evt) {
+  formState[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_FEEDBACK_FORM, JSON.stringify(formState));
+}
+function onFeedbackFormSubmit(evt) {
+  evt.preventDefault();
+  const feedbackFormData = {
+    email: evt.target.email.value,
+    message: evt.target.message.value,
+  };
+  console.log(feedbackFormData);
+  localStorage.removeItem(STORAGE_FEEDBACK_FORM);
+  evt.target.reset();
+}
